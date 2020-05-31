7616845275895663f19586a08b09167ff9faffd9
rylienellis <57364564+rylienellis@users.noreply.github.com>
2020-05-31 16:36:02 -0400

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,6 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
+    // Filters the data so that it retrieves the month and year combo -- can't have one without the other
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.years = ["2016", "2017", "2018", "2019"];
     exports.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
