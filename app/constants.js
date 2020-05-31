b782c1fe72ae7b40bd2ea44ced14829456f390dc
rylienellis <57364564+rylienellis@users.noreply.github.com>
2020-05-31 16:33:02 -0400

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -2,5 +2,6 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.years = ["2016", "2017", "2018", "2019"];
+    exports.months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 });
 //# sourceMappingURL=constants.js.map
