8ea0074acdf7e9937277df1b55dbdc8c2093d780
rylienellis <57364564+rylienellis@users.noreply.github.com>
2020-06-03 19:49:31 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -260,7 +260,8 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                                   type: "pie-chart",
                                   caption: "",
                                   value: {
-                                    fields: ["Band_Owned", "Emergency_Shelter", "On_the_Street", "Rooming_House", "Own_Home", "Private_Rental", "Social_Housing", "Family_or_Friends", "Youth_Home_Shelter", "Unknown_Housing"]
+                                    fields: ["Band_Owned", "Emergency_Shelter", "On_the_Street", "Rooming_House", "Own_Home", "Private_Rental", "Social_Housing", "Family_or_Friends", "Youth_Home_Shelter", "Unknown_Housing"],
+                                    normalizeField: null
                                   }
                                 }
                               ]
@@ -273,8 +274,8 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                                   type: "pie-chart",
                                   caption: "",
                                   value: {
-                                    fields: [ "Canada_Child_Benefit", "Disability_Benefits", "Employment", "Employment_Insurance", "No_Income", "Pension", "Provincial_Disability", "Social_Assistance", "Student_Loan", "Unknown_Income" ]
-
+                                    fields: [ "Canada_Child_Benefit", "Disability_Benefits", "Employment", "Employment_Insurance", "No_Income", "Pension", "Provincial_Disability", "Social_Assistance", "Student_Loan", "Unknown_Income"],
+                                    normalizeField: null
                                   }
                                 }
                               ]
